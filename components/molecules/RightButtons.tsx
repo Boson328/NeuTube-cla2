0bf547090597f7372ee37b072382d3a7236324f7
boson328 <boson_typing@outlook.jp>
2023-07-27 05:27:05 +0900

    componentの場所変更

---
+++ components/molecules/RightButtons.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+
+import AccountIcon from "@/components/atoms/AccountIcon";
+import Notifications from "@/components/atoms/Notifications";
+
+export default function RightButtons() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Notifications />
+      <AccountIcon src="https://lh3.googleusercontent.com/a/AAcHTtcSjnjYf6a-pKUn9hORsulXzo1_dmjyG2fLazsv=s524-c-no" />
+    </Box>
+  );
+}

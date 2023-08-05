9add314d864e584589e5f707074460e18d9f59cd
boson328 <boson_typing@outlook.jp>
2023-08-05 15:42:54 +0900

    nanka

--- utils/atoms.ts
+++ utils/atoms.ts
@@ -1,11 +1,11 @@
-import type { Settings } from "./types";
+import type { SettingsType } from "./types";
 
 import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 export const timeAtom = atom(0);
 export const wordIdxAtom = atom(0);
-export const settingsAtom = atomWithStorage<Settings>("settings", {
+export const settingsAtom = atomWithStorage<SettingsType>("settings", {
   keyboard: 0,
   volume: 50
 });

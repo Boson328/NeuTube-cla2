968ab7f3d6ea6560b506d6aa0164a6a94f7c58d4
boson <boson_typing@outlook.jp>
2023-12-23 21:51:15 +0900

    refirst commit

---
+++ packages/frontend/app/root.tsx
@@ -0,0 +1,33 @@
+import tailwind from "./tailwind.css"
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => [
+  ...(tailwind ? [{ rel: "stylesheet", href: tailwind }] : []),
+];
+
+export default function App() {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
